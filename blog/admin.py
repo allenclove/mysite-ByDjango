1d3001b321830c381b890ba1f5f5268792367b04
allenclove <1393623445@qq.com>
2019-07-09 18:00:34 +0800

    初始化项目

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import BlogType, Blog
+@admin.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+	list_display = ('id','type_name','is_display')
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'blog_type', 'author','get_read_num', 'created_time', 'last_updated_time')
