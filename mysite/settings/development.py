73c3ed4eb13fb834fade3ddda72812df4934348a
allenclove <1393623445@qq.com>
2019-07-19 15:47:59 +0800

    改回ssl方式发邮件，465端口

--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -47,10 +47,10 @@ DATABASES = {
 #发送邮件设置
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
-EMAIL_PORT = 25
+EMAIL_PORT = 465
 EMAIL_HOST_USER = '1393623445@qq.com'
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']  #授权码
 EMAIL_SUBJECT_PREFIX = '[allenc的博客]'
-EMAIL_USE_TLS = True  #与SMTP服务器通信时，是否启动TLS链接(安全链接)
+EMAIL_USE_TSSL = True  #与SMTP服务器通信时，是否启动SSL链接(安全链接)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
