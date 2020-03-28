d78677e0a4852991c5068cc0c6339b9ba1e0e268
allenclove <1393623445@qq.com>
2020-03-28 13:27:41 +0800

    增加和完善了lolskin页面

--- skin_box/urls.py
+++ skin_box/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #http://localhost:8000/blog/
     path('', views.lolskin, name='lolskin'),
     path('get_access_token/', views.get_access_token, name='get_access_token'),
+    path('get_download_url/', views.get_download_url, name='get_download_url'),
 ]
--- skin_box/views.py
+++ skin_box/views.py
@@ -2,22 +2,11 @@ import re
 import requests
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.shortcuts import redirect
 # Create your views here.
 
 #皮肤助手页面
 def lolskin(request):
-	'''
-	#这段代码只要软件首页被访问就运行,获取网页下载网址
-	html = requests.get("http://leagueskin.net/p/download-mod-skin-2020-chn")
-	content = str(html.content,encoding="utf-8")
-	#print(html.content)
-	skinbox_url_list = re.findall(r"<a id=\"link_download3\" href=\"(.*)\"> <button style=",content)    #正则获取皮肤盒子下载地址
-	skinbox_url = str(skinbox_url_list)            #转化为字符串
-	#print(b)
-	#print(type(b))
-	context = {}
-	context['skinbox_url'] = skinbox_url_list
-	'''
 	context = {}
 	return render(request, 'lolskin.html', context)
 
@@ -30,3 +19,14 @@ def get_access_token(self):
 	data = {}
 	data['access_token'] = access_token
 	return JsonResponse(data)
+
+def get_download_url(request):
+		#这段代码只要软件首页被访问就运行,获取网页下载网址
+	html = requests.get("http://leagueskin.net/p/download-mod-skin-2020-chn")
+	content = str(html.content,encoding="utf-8")
+	#print(html.content)
+	skinbox_url_list = re.findall(r"<a id=\"link_download3\" href=\"(.*)\"> <button style=",content)    #正则获取皮肤盒子下载地址
+	skinbox_url = ''.join(skinbox_url_list)            #转化为字符串,只有一个元素也就是下载网址
+	#print(skinbox_url)
+	#print(skinbox_url_list)
+	return redirect(skinbox_url)
